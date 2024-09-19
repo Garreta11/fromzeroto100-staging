05c89fd7c7531c6d02cd2240b589ef35c9c3a5e8
Jordi Garreta <jordigarreta11@gmail.com>
2024-09-19 12:09:35 +0200

    SuspenseWrapper on page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,17 @@ import PageWrapper from "./components/PageWrapper/PageWrapper";
 import styles from "./page.module.scss";
 
 import { DataProvider } from './contexts/DataContext';
+import SuspenseWrapper from './components/SuspenseWrapper/SuspenseWrapper';
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <DataProvider>
-        <PageWrapper />
-      </DataProvider>
-    </div>
+    <SuspenseWrapper>
+      <div className={styles.page}>
+        <DataProvider>
+          <PageWrapper />
+        </DataProvider>
+      </div>
+    </SuspenseWrapper>
   );
 }
